b63fa5ccc57709bcfb9d96e6b3aaf50dcfcf85c9
Marcel König <102319599+mkoenig3@users.noreply.github.com>
2023-04-25 18:41:24 -0700

    added automatic subpackage integration

--- water_quality/__init__.py
+++ water_quality/__init__.py
@@ -1,1 +1,12 @@
-__all__ = ["atmosphere","data","helper","model","surface","water"]
+# __all__ = ["atmosphere","data","helper","model","surface","water"]
+
+import os
+import importlib
+
+subdirectories = [name for name in os.listdir(os.path.dirname(__file__))
+                  if os.path.isdir(os.path.join(os.path.dirname(__file__), name))]
+
+for subdirectory in subdirectories:
+    package_path = f"{__name__}.{subdirectory}"
+    package = importlib.import_module(package_path)
+    globals().update(vars(package))
